828e1e307730202c3255e57981fb36238752d6c7
Towhidul Islam <towhidulislam2.bd@gmail.com>
2023-08-18 00:02:33 +0600

    news section + product card ==> addtocard + shopkeeperProfileCV done

--- client/src/components/NewsFeedInput/NewsFeedInput.jsx
+++ client/src/components/NewsFeedInput/NewsFeedInput.jsx
@@ -1,13 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import { FaFileUpload } from "react-icons/fa";
 import { FaCamera } from "react-icons/fa6";
-
-
+import{ useRef } from "react";
+import JoditEditor from "jodit-react";
 const NewsFeedInput = () => {
-    const [cameraError, setCameraError] = useState(null);
-
+  const [cameraError, setCameraError] = useState(null);
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
+  // const config = useMemo(
+  //   {
+  //     readonly: false, // all options from https://xdsoft.net/jodit/docs/,
+  //     placeholder:  "Start typings...",
+  //   },
+  //   []
+  // );
   const handleCameraClick = async () => {
+   
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
 
@@ -20,41 +30,48 @@ const NewsFeedInput = () => {
       setCameraError("Camera access denied. Please grant camera permission.");
     }
   };
-    return (
-        <div>
-            <div className="flex items-center p-4 border border-gray-200 rounded-lg">
-              <div className="mr-4">
-                <FaCamera
-                  className="text-2xl cursor-pointer"
-                  onClick={handleCameraClick}
-                />
-              </div>
-              <div className="flex-grow">
-                <input
-                  type="text"
-                  id=""
-                  className="w-full border-none bg-transparent placeholder-gray-500 focus:outline-none"
-                  placeholder="Write post"
-                />
-              </div>
-              <div className="ml-4 flex items-center">
-                <label
-                  className="cursor-pointer"
-                  htmlFor="fileInput"
-                >
-                  <FaFileUpload className="text-2xl" />
-                </label>
-                <input
-                  type="file"
-                  id="fileInput"
-                  className="hidden"
-                />
-              </div>
-            </div>
-            {cameraError && <p className="text-red-500 mt-2">{cameraError}</p>}
-            <div className="divider"></div>
+  return (
+    <div onClick={() => window.my_modal_3.showModal()}>
+      <div className="flex items-center p-4 border border-gray-200 rounded-lg">
+        <div className="mr-4">
+          <FaCamera
+            className="text-2xl cursor-pointer"
+            onClick={handleCameraClick}
+          />
         </div>
-    );
+        <div className="flex-grow">
+          <input
+            type="text"
+            id=""
+            className="w-full border-none bg-transparent placeholder-gray-500 focus:outline-none"
+            placeholder="Write post"
+            disabled
+          />
+        </div>
+        <div className="ml-4 flex items-center">
+          <label className="cursor-pointer" htmlFor="fileInput">
+            <FaFileUpload className="text-2xl" />
+          </label>
+          <input type="file" id="fileInput" className="hidden" />
+        </div>
+      </div>
+      {cameraError && <p className="text-red-500 mt-2">{cameraError}</p>}
+      <div className="divider"></div>
+      <dialog id="my_modal_3" className="modal">
+        <form method="dialog" className="modal-box">
+        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+          <JoditEditor
+            ref={editor}
+            value={content}
+            // config={config}
+            tabIndex={100} // tabIndex of textarea
+            onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+            onChange={(newContent) => setContent(newContent)}
+          />
+        </form>
+      </dialog>
+    </div>
+  );
 };
 
 export default NewsFeedInput;
--- client/src/components/ProductCart/ProductCart.jsx
+++ client/src/components/ProductCart/ProductCart.jsx
@@ -4,20 +4,21 @@ import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
 import MainProduct from "./MainProduct";
 
-const ProductCart = ({productName}) => {
+const ProductCart = ({product}) => {
+  const {name,price}=product
   const profileImageUrl =
     "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80";
   const storeName = "Nayen shop";
   const rating = 3;
 
 //   const productName = "Bombay Sweets Ring Chips";
-  const productImage =
-    "https://img.freepik.com/free-vector/realistic-chips-package_1284-34786.jpg?w=826&t=st=1691298851~exp=1691299451~hmac=f0f2f77684302248188778a32b0a8acd676af9a5e21ad7be5c15d05cbf5639c8";
+  // const productImage =
+  //   "https://img.freepik.com/free-vector/realistic-chips-package_1284-34786.jpg?w=826&t=st=1691298851~exp=1691299451~hmac=f0f2f77684302248188778a32b0a8acd676af9a5e21ad7be5c15d05cbf5639c8";
   return (
-    <div className=" w-[500px] border border-gray-100 mx-auto rounded-md">
-      <div className="p-1">
+    <div className=" w-[300px] border border-gray-100 mx-auto rounded-md">
+      <div className="">
         <div className="flex items-center justify-between">
-          <div className="flex gap-3 items-center">
+          <div className="flex gap-3 items-center px-2 py-2">
             <img
               className="w-10 h-10 rounded-full"
               src={profileImageUrl}
@@ -38,7 +39,7 @@ const ProductCart = ({productName}) => {
           </div>
           <div className="">
             <Rating
-              style={{ maxWidth: 150 }}
+              style={{ maxWidth: 75 }}
               readOnly
               orientation="horizontal"
               value={rating}
@@ -46,7 +47,7 @@ const ProductCart = ({productName}) => {
           </div>
         </div>
       </div>
-      <MainProduct productImage={productImage} productName={productName}></MainProduct>
+      <MainProduct product={product}></MainProduct>
     </div>
   );
 };
