0a7bcca58b09bfab0c0bd92256281993228c087f
tanzim-nahid <tanzimnahid6@gmail.com>
2024-04-07 00:10:50 +0600

    delivery charge added

--- client/src/components/PaymentMethod/Checkout/OrderSummery.jsx
+++ client/src/components/PaymentMethod/Checkout/OrderSummery.jsx
@@ -1,41 +1,45 @@
 import { Takaicon } from "@SvgHub/SocialIcon";
 
-const OrderSummary = ({ totalPrice, discount, totalItem }) => {
-    return (
-        <div className="mt-4 ">
-            <span className="my-8 text-xl font-semibold">Order Summary</span>
-            <div className="bg-gray-100 p-4 rounded-md">
-                <div className="flex items-center justify-between text-base font-medium text-gray-800 mb-2">
-                    <span>Total ({totalItem.length} Items)</span>
-                    <div className="flex items-center gap-2">
-                        <Takaicon />
-                        <span>{totalPrice}</span>
-                    </div>
-                </div>
-                <div className="flex items-center justify-between text-base font-medium text-gray-800 mb-2">
-                    <span>Discount</span>
-                    <div className="flex items-center gap-2">
-                        <Takaicon />
-                        <span>{discount ? discount : 0}</span>
-                    </div>
-                </div>
-                <div className="flex items-center justify-between text-base font-medium text-gray-800 mb-2">
-                    <span className="text-blue-600">Delivery Charges</span>
-                    <div className="flex items-center gap-2">
-                        <Takaicon />
-                        <span>0</span>
-                    </div>
-                </div>
-                <div className="my-4 flex items-center justify-between text-lg font-bold text-gray-800 border-t pt-4">
-                    <span>Grand Total</span>
-                    <div className="flex items-center gap-2">
-                        <Takaicon />
-                        <span>{+totalPrice - (+discount ? +discount : 0)}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+const OrderSummary = ({ totalPrice, discount, totalItem, deliveryCharge }) => {
+	return (
+		<div className="mt-4 ">
+			<span className="my-8 text-xl font-semibold">Order Summary</span>
+			<div className="rounded-md bg-gray-100 p-4">
+				<div className="mb-2 flex items-center justify-between text-base font-medium text-gray-800">
+					<span>Total ({totalItem.length} Items)</span>
+					<div className="flex items-center gap-2">
+						<Takaicon />
+						<span>{totalPrice}</span>
+					</div>
+				</div>
+				<div className="mb-2 flex items-center justify-between text-base font-medium text-gray-800">
+					<span>Discount</span>
+					<div className="flex items-center gap-2">
+						<Takaicon />
+						<span>{discount ? discount : 0}</span>
+					</div>
+				</div>
+				<div className="mb-2 flex items-center justify-between text-base font-medium text-gray-800">
+					<span className="text-blue-600">Delivery Charges</span>
+					<div className="flex items-center gap-2">
+						<Takaicon />
+						<span>{deliveryCharge}</span>
+					</div>
+				</div>
+				<div className="my-4 flex items-center justify-between border-t pt-4 text-lg font-bold text-gray-800">
+					<span>Grand Total</span>
+					<div className="flex items-center gap-2">
+						<Takaicon />
+						<span>
+							{+totalPrice +
+								deliveryCharge -
+								(+discount ? +discount : 0)}
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default OrderSummary;
